9b600cc8fad98efe07c1358cbc9a1ab426421dd7
Ryan Kemmer <ryankemmer77@gmail.com>
2021-09-30 21:13:09 -0700

    update

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,7 +1,7 @@
 <?php 
 	$errors = '';
 	$limit_size=10000000;
-	$myemail = 'admin@wordpressboss.com'; /*Replace with your email*/
+	$myemail = 'ryankemmer77@gmail.com'; /*Replace with your email*/
 	if(empty($_POST['name'])  ||
 	   empty($_POST['email']) ||
 	   empty($_POST['message']))
